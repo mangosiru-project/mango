aeb02fe83daa42e2f37a8ff1ef0f1eb3e70cc1cd
imyeyun <98500675+imyeyun@users.noreply.github.com>
2024-06-16 20:27:59 +0900

    해정이거 커밋

--- demo/src/main/java/com/example/demo/member/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/member/controller/HomeController.java
@@ -2,5 +2,14 @@ package com.example.demo.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
+    @GetMapping("/community")
+    public String community() {
+        return "community";
+    }
+    @GetMapping("/story")
+    public String story() {
+        return "story";
+    }
 }
